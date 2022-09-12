04bab7cd077972ed2d0e217e688007a8cab854f6
tsr07 <97617007+tsr07@users.noreply.github.com>
2022-09-12 10:56:51 +0700

    Update runLed.c

--- setIO/runLed.c
+++ setIO/runLed.c
@@ -2,26 +2,44 @@
 #define F_CPU 1000000UL
 #include <util/delay.h>
 
+int main();
+void setIO();
+void runKasus1();
+void runKasus2();
+
 int main(void)
 {
-	
-    DDRB = 0xFF; // SetOutput
-	
-    while (1) 
-    {
-		PORTB = 0x80;
-		for (int i = 0; i < 8; i++)
-		{
-			PORTB >>= 1;
-			PORTB |= 0x80;
-			_delay_ms(100);
-		}
-		for (int i = 0; i < 8; i++)
-		{
-			PORTB <<= 1;
-			PORTB |= 0x00;
-			_delay_ms(100);
-		}
-		
-    }
+	setIO();
+	while (1) { runKasus1(); }
+}
+
+void setIO() 
+{
+	DDRB = 0xFF; // SetOutput
+	PORTB = 0x80;
+}
+
+void runKasus1() 
+{
+	for (int i = 0; i < 14; i++)
+	{
+		if (i > 6) PORTB <<= 1;
+		else PORTB >>= 1;
+		_delay_ms(100);
+	}
+}
+
+void runKasus2() 
+{
+	for (int i = 0; i < 8; i++)
+	{
+		PORTB >>= 1;
+		PORTB |= 0x80;
+		_delay_ms(100);
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		PORTB <<= 1;
+		_delay_ms(100);
+	}
 }
