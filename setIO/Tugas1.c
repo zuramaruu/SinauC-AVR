c2748779592e72cc3b51e067f6276fe8e1147c9a
Juramaru <109219513+zuramaruu@users.noreply.github.com>
2022-09-18 21:07:44 +0700

    Create Tugas1.c

---
+++ setIO/Tugas1.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#define F_CPU 1000000UL
+#include <util/delay.h>
+
+int main();
+void setIO();
+void runKasus1();
+void runKasus2();
+
+int main(void)
+{
+	setIO(); // SetOutput
+	while (1) { runTugas1(); } // Infitinite Loop()
+}
+
+void setIO() 
+{
+	DDRB = 0xFF;
+	PORTB = 0x80;
+}
+
+void runTugas1() 
+{
+	for (int i = 0; i < 14; i++)
+	{
+		if (i > 6) PORTB <<= 1;
+		else PORTB >>= 1;
+		_delay_ms(100);
+	}
+}
