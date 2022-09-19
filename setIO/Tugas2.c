951faf0d9fcc20665c5fa8afdabf2ae39389e415
Juramaru <109219513+zuramaruu@users.noreply.github.com>
2022-09-19 18:24:35 +0700

    Update Tugas2.c

--- setIO/Tugas2.c
+++ setIO/Tugas2.c
@@ -4,31 +4,29 @@
 
 int main();
 void setIO();
-void runKasus2();
+void runTugas2();
 
 int main(void)
 {
 	setIO(); // SetOutput
-	while (1) { runKTugas2(); } // Infitinite Loop()
+	while (1) { runTugas2(); } // Infitinite Loop()
 }
 
-void setIO() 
+void setIO()
 {
 	DDRB = 0xFF;
 	PORTB = 0x80;
 }
 
-void runKTugas2() 
+void runTugas2()
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 14; i++)
 	{
-		PORTB >>= 1;
-		PORTB |= 0x80;
-		_delay_ms(100);
-	}
-	for (int i = 0; i < 8; i++)
-	{
-		PORTB <<= 1;
-		_delay_ms(100);
+		if (i > 6) PORTB <<= 1;
+		else {
+			PORTB >>= 1;
+			PORTB |= 0x80;
+		}
+		_delay_ms(100);	
 	}
 }
