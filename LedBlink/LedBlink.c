7e1b7375df14dbc0c487b12e90ae7502261d7ee0
Juramaru <109219513+zuramaruu@users.noreply.github.com>
2022-09-23 11:00:14 +0700

    Update LedBlink.c

--- LedBlink/LedBlink.c
+++ LedBlink/LedBlink.c
@@ -2,44 +2,44 @@
 #define __AVR_ATmega32__
 #endif
 
-#include <avr/io.h>
-#define F_CPU 1000000UL
-#include <util/delay.h>
+#include "avr/io.h"
+#define  F_CPU 1000000UL
+#include "util/delay.h"
+#include "Pins.h"
+
+#define vchar   volatile unsigned char
+#define NOL     0xBF
+#define DUA     0xDB
+
+#define DIG1    0x08
+#define DIG2    0x04
+#define DIG3    0x02
+#define DIG4    0x01
 
 int i;
+char arr[] = {DUA, NOL, DUA, NOL};
+char ord[] = {DIG1, DIG2, DIG3, DIG4};
 
-int main();
+int  main();
 void setIO();
 void loop();
+void set(vchar *, char num,vchar * ,  char pos);
 
-int main(void) { setIO(); while (1) { loop(); } }
+int main(void) { setup(); while (1) { loop(); } }
 
-void setIO()
-{
-	DDRB = 0xFF;
-        DDRC = 0xFF;
-        // PORTB = 0x80;
+void setup() {
+	pinModeB = Output;
+        pinModeC = Output;
 }
 
-void loop()
-{
-	for (i = 1; i < 5; i++) {
-                switch (i) {
-                case 1: zero(); break;
-                case 2: two(); break;
-                case 3: zero(); break;
-                case 4: two(); break;
-                }
-		_delay_ms(1000);	
+void loop() {
+	for (i = 0; i < 4; i++) {
+                set(&setC, arr[i], &setB , ord[i]);
+		_delay_ms(600);	
 	}
 }
 
-void two() {
-        PORTB = 0x01;
-        PORTC = 0xDB;
-}
-
-void zero() {
-        PORTB = 0x08;
-        PORTC = 0xBF;
+void set(vchar *port_x, char num, vchar *port_y, char pos) {
+        *port_x = num;
+        *port_y = pos;
 }
